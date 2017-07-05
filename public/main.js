c7f92772bb43a50f4b73a7ef6582257d44a39117
PDN-04 <domingueznavarropablo@gmail.com>
2017-07-05 21:57:36 +0200

    Versión 0.2

--- public/main.js
+++ public/main.js
@@ -1,30 +1,23 @@
-// Conexión del cliente con el servidor de WebSockets
-var socket = io.connect('http://localhost:8080', { 'forceNew': true });
+$(document).ready(function() {
+    var socket = io.connect("http://localhost:8080", { "forceNew": true });
 
-// Servidor atento al evento 'messages' (los mensajes del chat)
-// La variable 'data' contiene todos los mensajes que envía el servidor
-socket.on('messages', function(data) {
-    // Mostrar a través de HTML los mensajes del chat
-    mostrarMensajes(data);
+    socket.on("all-messages", function(data) {
+        var html = data.map(function(elemento) {
+            return "<div>" + "<strong>" + elemento.usuario + "</strong>: " + "<em>" + elemento.mensaje + "</em>" + "</div>";
+        }).join(" ");
+        $("#mensajes").html(html);
+    });
+
+    $("form").submit(function(evento) {
+        evento.preventDefault();
+
+        var data = {
+            usuario: "",
+            mensaje: $("#texto").val()
+        };
+
+        if (data.mensaje != "") {
+            socket.emit("new-message", data);
+        }
+    });
 });
-
-// Crea la estructura HTML de los mensajes a partir del array 'mensajes'
-function mostrarMensajes(data) {
-    var html = data.map(function(elem, index) {
-        return "<div>" + "<strong>" + elem.author + "</strong>: " + "<em>" + elem.text + "</em>" + "</div>";
-    }).join(" ");
-
-    document.getElementById('mensajes').innerHTML = html;
-}
-
-// Recoge los valores obtenidos del formulario y los envía por el servidor de WebSockets para que lo escuche el servidor
-function anadirMensaje(evento) {
-    // Nuevo mensaje en formato JSON
-    var mensaje = {
-        author: document.getElementById('username').value,
-        text: document.getElementById('texto').value
-    };
-    // Enviar mensaje al servidor de WebSockets a través del evento 'new-message'
-    socket.emit('new-message', mensaje);
-    return false;
-}
--- server/main.js
+++ server/main.js
@@ -1,43 +1,28 @@
-// Aplicación Express
-var express = require('express');
+var express = require("express");
 var app = express();
-// Servidor HTTP
-var server = require('http').Server(app);
-// Servidor de WebSockets
-var io = require('socket.io')(server);
-// Mensajes del chat de prueba
-var mensajes = [{
-    author: "Carlos",
-    text: "Hola! que tal?"
-}, {
-    author: "Pepe",
-    text: "Muy bien! y tu??"
-}, {
-    author: "Paco",
-    text: "Genial!"
-}];
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var mensajes = [];
 
-// Ruta de los ficheros públicos estáticos
 app.use(express.static('public'));
 
-// Servidor atento a nuevas conexiones de usuarios
-io.on('connection', function(socket) {
-    console.log('¡Alguien se ha conectado!');
-    // Emisión de mensajes del chat
-    socket.emit('messages', mensajes);
+io.on("connection", function(socket) {
+    console.log("El usuario " + socket.id + " se ha conectado.");
 
-    // Servidor atento a nuevos mensajes de los usuarios
-    socket.on('new-message', function(data) {
-        // Añadir el nuevo mensaje al array 'mensajes'
+    socket.emit("all-messages", mensajes);
+
+    socket.on("new-message", function(data) {
+        data.usuario = socket.id;
         mensajes.push(data);
-        // Emitir los mensajes a todos los usuarios conectados
-        io.sockets.emit('messages', mensajes);
+        io.sockets.emit("all-messages", mensajes);
     });
+
+    socket.on("disconnect", function(event) {
+        console.log("El usuario " + socket.id + " se ha desconectado.");
+    });
+
 });
 
-
-
-// Servidor escuchando en el puerto 8080
-server.listen(8080, function() {
-    console.log('¡Chat en funcionamiento!');
+http.listen(8080, function() {
+    console.log("¡Chat en funcionamiento!");
 });
